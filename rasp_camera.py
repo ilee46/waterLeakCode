23c12c0f3889958945f9a1ae80d4c51cc3da2f75
lgarci95 <78031538+lgarci95@users.noreply.github.com>
2023-02-07 22:08:41 -0600

    Update rasp_camera.py

--- rasp_camera.py
+++ rasp_camera.py
@@ -1,6 +1,16 @@
 from picamera import Picamera
+from subprocess import call 
 
 camera = PiCamera()
-camera.start_recording('home/pi/test.h264')
-camera.stop_recording()
-#adding more soon
+def conversion(f_h264, f_mp4):
+  camera.start_recording('home/pi/test.h264')
+  camera.stop_recording()
+  command = "MP4Box -add " + f_h264 + " " + f_mp4
+  call([command], shell=true)
+  
+  
+#convert .h264 to .MP4 with MP4box
+# MP4 is needed for OpenCV format
+#'sudo apt install -y gpac'
+
+convert('home/pi/test.h264',''home/pi/test.mp4')
