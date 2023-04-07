c7a7a2977b8663fbf622e63eb83b1f0d75bbec2a
lgarci95 <78031538+lgarci95@users.noreply.github.com>
2023-04-06 23:29:54 -0500

    Delete videoprocess.py

--- videoprocess.py
+++
@@ -1,9 +0,0 @@
-import cv2
-water_cascade = cv2.CasacadeClassifier("water.xml")
-
-vid_capture = cv2.VideoCapture('/home/ianlee/test2.mp4')
-
-while True:
-    ret, img = vid_capture.read()
-    
-    
